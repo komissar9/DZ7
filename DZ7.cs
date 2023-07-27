620c42de197b750b853e95cbb0eae4098094ac12
Alena <magii.alena@yandex.ru>
2023-07-27 07:55:54 +0300

    Решение 50 задачи

--- DZ7.cs
+++ DZ7.cs
@@ -3,9 +3,9 @@ Console.Write("Введите количество строк: ");
 int rows = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количество cтолбцов: ");
 int columns = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите мин целое число: ");
+Console.Write("Введите мин целое число в массиве: ");
 int minRandom = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите макс целое число: ");
+Console.Write("Введите макс целое число в массиве: ");
 int maxRandom = Convert.ToInt32(Console.ReadLine());
 double[,] array2D = GetMatrix(rows, columns, minRandom, maxRandom);
 
@@ -16,7 +16,7 @@ double [,] GetMatrix (int m, int n, int min, int max)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i,j] = new Random().Next(min,max+1) + Math.Round(new Random().NextDouble(), 2);
+            matrix[i,j] = new Random().Next(min,max) + Math.Round(new Random().NextDouble(), 2);
         }
     }
     return matrix;
@@ -36,4 +36,18 @@ void PrintMatrix (double [,] matrix)
 PrintMatrix(array2D);
 
 // Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет
+// у меня эта задача просто продолжение предыдущей :)
+Console.Write("Введите № строки (отсчет начинается с 0): ");
+int rowsUser = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите № cтолбца (отсчет начинается с 0): ");
+int columnsUser = Convert.ToInt32(Console.ReadLine());
+if (rowsUser < rows & columnsUser < columns)
+{
+    Console.WriteLine(array2D[rowsUser, columnsUser]);
+}
+else
+{
+    Console.WriteLine("Такого элемента нет в массиве");
+}
+
 // Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце
